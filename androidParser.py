4f6f8093801ddee88921828905bf114d6ade25f4
Robert Serafin <rsera1@unh.newhaven.edu>
2021-03-22 04:51:52 -0400

    .

--- androidParser.py
+++ androidParser.py
@@ -1,6 +1,9 @@
 import os,tarfile,sys,datetime,pathlib,glob,re
 import xmltodict,sqlite3
 from columnar import columnar
+from shutil import copyfile
+import pandas as pd
+import hashlib
 
 timestamp = ""
 case = ""
@@ -57,25 +60,67 @@ def extract(file,user_list=None):
 	tar.close()
 
 def aauto():
-	print('Android Auto:\n')
-	try:
-		xml = xml_open('./{}/Settings/app_state_shared_preferences.xml'.format(foldername))
-	except:
-		print("./{}/Settings/app_state_shared_preferences.xml not found".format(foldername))
+	print("Android Auto:\n")
+	path = "./{}/Comm/carservicedata.db".format(foldername)
+	if not os.path.exists(path):
+		print("./{}/Comm/carservicedata.db not found.".format(foldername))
 		return
 
-	auto = {'Last_run':[]}
-	epoch = int(xml['map']['long']['@value'])
-	auto['Last_run'].append(datetime.datetime.utcfromtimestamp(epoch/1000).strftime("%m/%d/%Y %H:%M:%S UTC"))
-	
-	headers = auto.keys()
-	data = auto['Last_run']
-	tab_row = []
-	for i in range(len(data)):
-		tab_row.append([data[i]])
-	table = columnar(tab_row, headers)
-	print(table)
-	return auto
+	try:
+		connection = sqlite3.connect("./{}/Comm/carservicedata.db".format(foldername))
+		connection.text_factory = str
+		cursor = connection.cursor()
+		rows = cursor.execute("SELECT manufacturer,datetime(connectiontime/1000,'unixepoch'),vehicleid,bluetoothaddress FROM allowedcars").fetchall()
+		rows = rows[::-1]
+		aauto = {'manufacturer':[],'vehicle id':[],'last connection':[],'bluetooth address':[]}
+		for i in rows:
+			aauto['manufacturer'].append(i[0])
+			aauto['last connection'].append(i[1]+" UTC")
+			aauto['vehicle id'].append(i[2])
+			aauto['bluetooth address'].append(i[3])
+			
+
+		headers = list(aauto.keys())
+		data = list(aauto.values())
+		tab_row = []
+		for i in range(len(data[0])):
+			tab_row.append([data[0][i],data[1][i],data[2][i],data[3][i]])
+		table = columnar(tab_row, headers)
+		print(table)
+		return aauto
+
+	except:
+		print("Error with ./{}/Comm/carservicedata.db".format(foldername))
+
+def contacts():
+	print("Contacts:\n")
+	path = "./{}/Comm/contacts2.db".format(foldername)
+	if not os.path.exists(path):
+		print("./{}/Comm/contacts2.db not found.".format(foldername))
+		return
+
+	try:
+		connection = sqlite3.connect("./{}/Comm/contacts2.db".format(foldername))
+		connection.text_factory = str
+		cursor = connection.cursor()
+		rows = cursor.execute("SELECT M.data1 As Name, N.data1 As Number from data As M inner join data As N where (M.raw_contact_id=N.raw_contact_id and M.mimetype_id=7 and N.mimetype_id=5)").fetchall()
+		rows = rows[::-1]
+		cont = {'Name':[],'Number':[]}
+		for i in rows:
+			cont['Name'].append(i[0])
+			cont['Number'].append(i[1])
+
+		headers = list(cont.keys())
+		data = list(cont.values())
+		tab_row = []
+		for i in range(len(data[0])):
+			tab_row.append([data[0][i],data[1][i]])
+		table = columnar(tab_row, headers)
+		print(table)
+		return cont
+
+	except:
+		print("Error with ./{}/Comm/contacts2.db".format(foldername))
 
 def sms_history():
 	print("SMS History:\n")
@@ -91,14 +136,12 @@ def sms_history():
 		rows = cursor.execute("SELECT datetime(date/1000,'unixepoch'),address,type,body FROM sms").fetchall()
 		rows = rows[::-1]
 		sms = {'timestamp':[],'number':[],'origin':[],'text':[]}
+		types = {1:'Received',2:'Sent',5:'Failed to send'}
 		for i in rows:
 			sms['timestamp'].append(i[0]+" UTC")
 			sms['number'].append(i[1])
 			sms['text'].append(i[3])
-			if i[2] == 1:
-				sms['origin'].append("Sent")
-			else:
-				sms['origin'].append("Received")
+			sms['origin'].append(types[i[2]])
 		
 		headers = list(sms.keys())
 		data = list(sms.values())
@@ -123,19 +166,21 @@ def call_history():
 		connection = sqlite3.connect("./{}/Comm/calllog.db".format(foldername))
 		connection.text_factory = str
 		cursor = connection.cursor()
-		rows = cursor.execute("SELECT datetime(date/1000,'unixepoch'),datetime(date/1000+duration,'unixepoch'),logtype,number FROM calls").fetchall()
+		rows = cursor.execute("SELECT datetime(date/1000,'unixepoch'),datetime(date/1000+duration,'unixepoch'),number,name,type FROM calls WHERE logtype == 100 ").fetchall()
 		rows = rows[::-1]
-		calls = {'timestamp_duration':[],'number':[]}
+		calls = {'timestamp_duration':[],'type':[],'contact':[],'number':[]}
+		types = {1:'Incoming',2:'Outgoing',3:'Missed',5:'Rejected'}
 		for i in rows:
-			if i[2] == 100:
-				calls['timestamp_duration'].append(i[0]+" - "+i[1]+" UTC")
-				calls['number'].append(i[3])
+			calls['timestamp_duration'].append(i[0]+" - "+i[1]+" UTC")
+			calls['number'].append(i[2])
+			calls['contact'].append(i[3])
+			calls['type'].append(types[i[4]])
 		
 		headers = list(calls.keys())
 		data = list(calls.values())
 		tab_row = []
 		for i in range(len(data[0])):
-			tab_row.append([data[0][i],data[1][i]])
+			tab_row.append([data[0][i],data[1][i],data[2][i],data[3][i]])
 		table = columnar(tab_row, headers)
 		print(table)
 		return calls
@@ -147,6 +192,7 @@ def voice_commands():
 	print("Android Assistant")
 	pb = glob.glob('./{}/Voice/*.binarypb'.format(foldername))
 	out_arr = {}
+	incars = []
 	for file in pb:
 		with open(file, "rb") as f:
 		    data = f.read()
@@ -160,6 +206,11 @@ def voice_commands():
 		    matches.append(m.group(0))#curpos + m.start())
 		    curpos += m.end()
 
+		if len(matches) != 0:
+			if data[16:29] == b'car_assistant':
+				incars.append('Yes')
+			else:
+				incars.append('No')
 
 		for i in matches:
 			s = ""
@@ -185,70 +236,90 @@ def voice_commands():
 				else:
 				  out_arr.update({file[17:]:[]})
 				  out_arr[file[17:]].append(cmd)
-
-					
+	cmds = {'File':[],'User_commands':[],'In Car':incars}				
 	headers = list(out_arr.keys())
 	data = list(out_arr.values())
 	tab_row = []
 	for i in range(len(headers)):
-		tab_row.append([headers[i],str.join("\n",data[i])])
-		#print(str.join("\n",data[i]))
-	table = columnar(tab_row,)
+		tab_row.append([headers[i],str.join("\n",data[i]),incars[i]])
+		cmds['File'].append(headers[i])
+		cmds['User_commands'].append(data[i])
+	table = columnar(tab_row,cmds.keys())
 	print(table)
-	return out_arr
+	return cmds
 
-def report(cmds,pair,call,sms):
+def report(voice,sett,cont,call,sms):
 	copyfile('./style.css', './{}/style.css'.format(foldername))
 	f = open("./{}/report.html".format(foldername),"w")
-	f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'><title>repl.it</title>\
-		     <link href='style.css' rel='stylesheet' type='text/css' /></head><body><h1>Apple Carplay Forensics Report</h1>\
+	f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'><title>Android Auto</title>\
+		     <link href='style.css' rel='stylesheet' type='text/css' /></head><body><h1>Android Auto Forensics Report</h1>\
 		     <div style='margin-left:auto;margin-right:auto;width:800px;height:250px;border:2px solid #000;'><h3>\
 		     Filename: {}<br>Case: {}<br>Timestamp: {}<br>Examiner: {}<br>Before Analysis:<br>MD5: {}<br>SHA256: {}<br>After Analysis:<br>MD5: {}<br>SHA256: {}</h3></div>".format(foldername,case,timestamp,examiner,md5,sha256,check_md5,check_sha256))
-	if not cmds:
+	
+	if not voice:
 		pass
 	else:
-		f.write("<h2>Recent Siri Voice Commands</h2>\n")
-		df3 = pd.DataFrame(cmds)
-		df3 = df3.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t03')
-		df3 = df3.replace("T_t03","t03")
+		f.write("<h2>Recent Android Assistant Voice Commands</h2>\n")
+		df1 = pd.DataFrame(voice)
+		df1 = df1.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t03')
+		df1 = df1.replace("T_t03","t03")
+		f.write(df1)
+
+	if not sett:
+		pass
+	else:
+		f.write("<h2>Android Auto</h2>\n")
+		df2 = pd.DataFrame(sett)
+		df2 = df2.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t04')
+		df2 = df2.replace("T_t04","t04")
+		f.write(df2)
+
+	if not cont:
+		pass
+	else:
+		f.write("<h2>Contacts</h2>\n")
+		df3 = pd.DataFrame(cont)
+		df3 = df3.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t04')
+		df3 = df3.replace("T_t04","t04")
 		f.write(df3)
 
-	f.write("<h2>Carplay Pairings</h2>\n")
-	df4 = pd.DataFrame(pair)
-	df4 = df4.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t04')
-	df4 = df4.replace("T_t04","t04")
-	f.write(df4)
-	f.write("<h2>Recent SMS</h2>\n")
-	df2 = pd.DataFrame(sms)
-	df2 = df2.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t02')
-	df2 = df2.replace("T_t02","t02")
-	f.write(df2)
-	f.write("<h2>Recent Phone Calls</h2>\n")
-	df1 = pd.DataFrame(call)
-	df1 = df1.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t01')
-	df1 = df1.replace("T_t01","t01")
-	f.write(df1)
+	if not call:
+		pass
+	else:
+		f.write("<h2>Recent Phone Calls</h2>\n")
+		df4 = pd.DataFrame(call)
+		df4 = df4.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t01')
+		df4 = df4.replace("T_t01","t01")
+		f.write(df4)
+
+	if not sms:
+		pass
+	else:
+		f.write("<h2>Recent SMS</h2>\n")
+		df5 = pd.DataFrame(sms)
+		df5 = df5.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t02')
+		df5 = df5.replace("T_t02","t02")
+		f.write(df5)
+	
 	f.write("</body></html>")
 	f.close()
 
-
 def report2():
 	f = open("./{}/report.html".format(foldername),"w")
-	f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'><title>repl.it</title>\
-		     <link href='style.css' rel='stylesheet' type='text/css' /></head><body><h1>Apple Carplay Forensics Report</h1>\
+	f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'><title>Android Auto</title>\
+		     <link href='style.css' rel='stylesheet' type='text/css' /></head><body><h1>Android Auto Forensics Report</h1>\
 		     <div style='margin-left:auto;margin-right:auto;width:800px;height:200px;border:2px solid #000;'><h3>\
 		     Filename: {}<br>Case: {}<br>Timestamp: {}<br>Examiner: {}<br>Before Analysis:<br>MD5: {}<br>SHA256: {}<br>After Analysis:<br>MD5: {}<br>SHA256: {}</h3></div>".format(foldername,case,timestamp,examiner,md5,sha256,check_md5,check_sha256))
-
 
 def setup(file):
 	global case,examiner,md5,sha256,foldername,timestamp
 	timestamp = datetime.datetime.utcnow().strftime("%m/%d/%Y-%H:%M:%S UTC")
 	case = input("Enter case #:")
-	foldername = "Android"#case+"Apple"
+	foldername = case+"-Android"
 	exmainer = input("Enter examiner name:")
-	md5 = "9fe5cfd3a3b7b89cd91b421112d4d86b"#hashlib.md5(open(file,'rb').read()).hexdigest()
+	md5 = hashlib.md5(open(file,'rb').read()).hexdigest()
 	print("MD5:",md5)
-	sha256 = "df73ffd582c3121630531feeabadf2ce29315ba42c7791646bf47c8ff20cb071"#hashlib.sha256(open(file,'rb').read()).hexdigest()
+	sha256 = hashlib.sha256(open(file,'rb').read()).hexdigest()
 	print("SHA256:",sha256)
 
 def hash_check(file):
@@ -271,10 +342,18 @@ def hash_check(file):
 		check_sha256 += " : Not Matched"
 
 def android(file,user_list=None):
-	setup(file)
-	#extract(file,user_list)
-	#sett = aauto()
-	#voice = voice_commands()
-	#call = call_history()
-	#sms = sms_history()
-	#report()
+	if user_list == None:
+		setup(file)
+		extract(file,user_list)
+		voice = voice_commands()
+		sett = aauto()
+		cont = contacts()
+		call = call_history()
+		sms = sms_history()
+		hash_check(file)
+		report(voice,sett,cont,call,sms)
+	else:
+		setup(file)
+		extract(file,user_list)
+		hash_check(file)
+		report2()
--- appleParser.py
+++ appleParser.py
@@ -8,6 +8,7 @@ import itertools
 import hashlib
 from shutil import copyfile
 
+#Data used for end report
 timestamp = ""
 case = ""
 foldername = ""
@@ -17,30 +18,30 @@ sha256 = ""
 check_md5 = ""
 check_sha256 = ""
 
-def extract(file,user_list=None):
-	wordlist = []
+def extract(file,user_list=None):											#Extracts files from wordlist
+	wordlist = []	
 	if user_list == None:
-		try:
-			with open('Default_lists/apple_default_list.txt') as my_file:
-				for line in my_file:
+		try:																#Tries to open the default wordlist
+			with open('Default_lists/apple_default_list.txt') as my_file:	
+				for line in my_file:										#Takes words in the wordlist file and creats a list
 					wordlist.append(line[:-1])
-		except FileNotFoundError:
+		except FileNotFoundError:											#File not found error
 			print("apple_default_list.txt not found")
-			sys.exit()
+			sys.exit()														#Exits program
 	else:
-		try:
+		try:																#Tries to open the user provided wordlist
 			with open(user_list) as my_file:
-				for line in my_file:
+				for line in my_file:										#Takes words in the wordlist file and creats a list
 					wordlist.append(line[:-1])
-		except FileNotFoundError:
+		except FileNotFoundError:											#File not found error
 			print("Specified wordlist not found")
-			sys.exit()
+			sys.exit()														#Exits program
 
-	tar = tarfile.open(file)
+	tar = tarfile.open(file)												#Opens the provided tar image
 	members = tar.getmembers()
 	names = tar.getnames()
 	
-	for name in names:
+	for name in names:														#Searches through the tar file to pull out matching files from the wordlist
 		for word in wordlist:
 			if word in name:
 				index = names.index(name)
@@ -51,7 +52,7 @@ def extract(file,user_list=None):
 						tar.extract(members[index], path='./{}/Settings'.format(foldername))
 					elif path.parent.name in ('Assistant','audio','PhotoAlbumNameType'):
 						tar.extract(members[index], path='./{}/Voice'.format(foldername))
-					elif path.parent.name in ('CallHistoryDB'):
+					elif path.parent.name in ('CallHistoryDB','AddressBook'):
 						tar.extract(members[index], path='./{}/Call_History'.format(foldername))
 					elif path.parent.name in ('SMS'):
 						tar.extract(members[index], path='./{}/SMS'.format(foldername))
@@ -60,24 +61,24 @@ def extract(file,user_list=None):
 	tar.close()
 
 def voice_commands():
-	wav = glob.glob('./{}/Voice/*.wav'.format(foldername))
+	wav = glob.glob('./{}/Voice/*.wav'.format(foldername))							#Makes a list of wav file names
 	r = sr.Recognizer()
 	print("No Siri audio file found.") if not wav else print("Siri Commands:")
-	
+
 	cmds = {'timestamp':[],'command':[]}
 	for file in wav:
 		exif = pathlib.Path(file).stat()
 		ctime = datetime.datetime.utcfromtimestamp(exif.st_ctime)
 		with sr.AudioFile(file) as source:
-		    audio_data = r.record(source)
-		    try:
-		    	text = r.recognize_google(audio_data)
-		    except:
-		    	print("Not connected to internet.")
-		    	return
-		    cmds['timestamp'].append(str(ctime) + " UTC")
-		    cmds['command'].append(text)
-		   
+			audio_data = r.record(source)
+			try:
+				text = r.recognize_google(audio_data)
+			except:
+				print("Not connected to internet.")
+				return
+			cmds['timestamp'].append(str(ctime) + " UTC")
+			cmds['command'].append(text)
+
 	headers = list(cmds.keys())
 	data = list(cmds.values())
 	tab_row = []
@@ -145,6 +146,62 @@ def carplay_pairings():
 	print(table)
 	return pairing
 
+def settings():
+	print('Settings:\n')
+	try:
+		cp = pl_open('./{}/Settings/com.apple.springboard.plist'.format(foldername))
+	except:
+		print("./{}/Settings/com.apple.springboard.plist not found.".format(foldername))
+		return
+
+	try:
+		cp2 = pl_open('./{}/Settings/com.apple.celestial.plist'.format(foldername))
+	except:
+		print("./{}/Settings/com.apple.celestial.plist not found.".format(foldername))
+		return
+
+	app = {'Recent Apps':[],'App Used Upon Disconnect':[]}
+	app['Recent Apps'].append(cp['CarDisplayRecentlyUsedApps'])
+	app['App Used Upon Disconnect'].append(cp2['nowPlayingAppDisplayIDUponCarPlayDisconnect'])
+
+	headers = list(app.keys())
+	data = list(app.values())
+	tab_row = []
+	for i in range(len(headers)):
+		tab_row.append([headers[i],data[i]])
+	table = columnar(tab_row,)
+	print(table)
+	return app
+
+def contacts():
+	print("Contacts:\n")
+	path = "./{}/Call_History/AddressBook.sqlitedb".format(foldername)
+	if not os.path.exists(path):
+		print("./{}/Call_History/AddressBook.sqlitedb not found.".format(foldername))
+		return
+
+	try:
+		connection = sqlite3.connect("./{}/Call_History/AddressBook.sqlitedb".format(foldername))
+		connection.text_factory = str
+		cursor = connection.cursor()
+		rows = cursor.execute("SELECT COALESCE(First,'') || ' ' || COALESCE(Middle,'') || ' ' || COALESCE(Last,'') As Name,ABMultiValue.value As Number from ABPerson INNER JOIN ABMultiValue ON ABMultiValue.record_id=ABPerson.ROWID;").fetchall()
+		rows = rows[::-1]
+		origin = {0:'Incoming',1:'Outgoing'}
+		cont = {'Name':[],'Number':[]}
+		for i in rows:
+			cont['Name'].append(i[0])
+			cont['Number'].append(i[1])
+
+		headers = list(cont.keys())
+		data = list(cont.values())
+		tab_row = []
+		for i in range(len(data[0])):
+			tab_row.append([data[0][i],data[1][i]])
+		table = columnar(tab_row, headers)
+		print(table)
+		return cont
+	except:
+		print("Error with ./{}/Call_History/AddressBook.sqlitedb".format(foldername))
 
 def call_history():
 	print("Phone Call History:\n")
@@ -181,7 +238,6 @@ def call_history():
 	except:
 		print("Error with ./{}/Call_History/CallHistory.storedata".format(foldername))
 
-
 def sms_history():
 	print("SMS History:\n")
 	path = "./{}/SMS/sms.db".format(foldername)
@@ -192,76 +248,109 @@ def sms_history():
 		connection = sqlite3.connect("./{}/SMS/sms.db".format(foldername))
 		connection.text_factory = str
 		cursor = connection.cursor()
-		rows = cursor.execute("SELECT  datetime(date/1000000000+978307200, 'unixepoch'),datetime(date_read+978307200, 'unixepoch'),date_read,text FROM message").fetchall()
+		rows = cursor.execute("SELECT  datetime(date/1000000000+978307200, 'unixepoch'),datetime(date_read+978307200, 'unixepoch'),date_read,is_sent,text FROM message").fetchall()
 		rows = rows[::-1]
-		sms = {'time_received':[],'time_read':[],'text':[]}
+		sms = {'time_received':[],'time_read':[],'origin':[],'text':[]}
+		origin = {0:'Recieved',1:'Sent'}
 		for i in rows:
 			sms['time_received'].append(i[0]+" UTC")
-			sms['text'].append(i[3])
-			if i[2] == 0:
-				sms['time_read'].append("SENT")
-			else:
+			if i[2] != 0:
 				sms['time_read'].append(i[1]+" UTC")
+			else:
+				sms['time_read'].append(" ")
+			sms['origin'].append(origin[i[3]])
+			sms['text'].append(i[4])
+			
 
 		headers = list(sms.keys())
 		data = list(sms.values())
 		tab_row = []
 		for i in range(len(data[0])):
-			tab_row.append([data[0][i],data[1][i],data[2][i]])
+			tab_row.append([data[0][i],data[1][i],data[2][i],data[3][i]])
 		table = columnar(tab_row, headers)
 		print(table)
 		return sms
 	except:
 		print("Error with ./{}/SMS/sms.db".format(foldername))
 
-def report(cmds,pair,call,sms):
+def report(cmds,pair,app,cont,call,sms):
 	copyfile('./style.css', './{}/style.css'.format(foldername))
 	f = open("./{}/report.html".format(foldername),"w")
-	f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'><title>repl.it</title>\
-		     <link href='style.css' rel='stylesheet' type='text/css' /></head><body><h1>Apple Carplay Forensics Report</h1>\
-		     <div style='margin-left:auto;margin-right:auto;width:800px;height:250px;border:2px solid #000;'><h3>\
-		     Filename: {}<br>Case: {}<br>Timestamp: {}<br>Examiner: {}<br>Before Analysis:<br>MD5: {}<br>SHA256: {}<br>After Analysis:<br>MD5: {}<br>SHA256: {}</h3></div>".format(foldername,case,timestamp,examiner,md5,sha256,check_md5,check_sha256))
+	f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'><title>Apple Carplay</title>\
+			<link href='style.css' rel='stylesheet' type='text/css' /></head><body><h1>Apple Carplay Forensics Report</h1>\
+			<div style='margin-left:auto;margin-right:auto;width:800px;height:250px;border:2px solid #000;'><h3>\
+			Filename: {}<br>Case: {}<br>Timestamp: {}<br>Examiner: {}<br>Before Analysis:<br>MD5: {}<br>SHA256: {}<br>After Analysis:<br>MD5: {}<br>SHA256: {}</h3></div>".format(foldername,case,timestamp,examiner,md5,sha256,check_md5,check_sha256))
+	
 	if not cmds:
 		pass
 	else:
 		f.write("<h2>Recent Siri Voice Commands</h2>\n")
-		df3 = pd.DataFrame(cmds)
-		df3 = df3.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t03')
-		df3 = df3.replace("T_t03","t03")
+		df1 = pd.DataFrame(cmds)
+		df1 = df1.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t03')
+		df1 = df1.replace("T_t03","t03")
+		f.write(df1)
+
+	if not pair:
+		pass
+	else:
+		f.write("<h2>Carplay Pairings</h2>\n")
+		df2 = pd.DataFrame(pair)
+		df2 = df2.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t04')
+		df2 = df2.replace("T_t04","t04")
+		f.write(df2)
+
+	if not app:
+		pass
+	else:
+		f.write("<h2>Carplay Apps</h2>\n")
+		df3 = pd.DataFrame(app)
+		df3 = df3.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t04')
+		df3 = df3.replace("T_t04","t04")
 		f.write(df3)
 
-	f.write("<h2>Carplay Pairings</h2>\n")
-	df4 = pd.DataFrame(pair)
-	df4 = df4.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t04')
-	df4 = df4.replace("T_t04","t04")
-	f.write(df4)
-	f.write("<h2>Recent SMS</h2>\n")
-	df2 = pd.DataFrame(sms)
-	df2 = df2.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t02')
-	df2 = df2.replace("T_t02","t02")
-	f.write(df2)
-	f.write("<h2>Recent Phone Calls</h2>\n")
-	df1 = pd.DataFrame(call)
-	df1 = df1.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t01')
-	df1 = df1.replace("T_t01","t01")
-	f.write(df1)
+	if not cont:
+		pass
+	else:
+		f.write("<h2>Contacts</h2>\n")
+		df4 = pd.DataFrame(cont)
+		df4 = df4.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t01')
+		df4 = df4.replace("T_t01","t01")
+		f.write(df4)
+	if not call:
+		pass
+	else:
+		f.write("<h2>Recent Phone Calls</h2>\n")
+		df5 = pd.DataFrame(call)
+		df5 = df5.style.set_properties(**{'text-align': 'center'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t01')
+		df5 = df5.replace("T_t01","t01")
+		f.write(df5)
+
+	if not sms:
+		pass
+	else:
+		f.write("<h2>Recent SMS</h2>\n")
+		df6 = pd.DataFrame(sms)
+		df6 = df6.style.set_properties(**{'text-align': 'left'},**{'width': '60px'}).set_table_attributes('class="center"').hide_index().render(uuid='t02')
+		df6 = df6.replace("T_t02","t02")
+		f.write(df6)
+
 	f.write("</body></html>")
 	f.close()
 
 
 def report2():
 	f = open("./{}/report.html".format(foldername),"w")
-	f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'><title>repl.it</title>\
-		     <link href='style.css' rel='stylesheet' type='text/css' /></head><body><h1>Apple Carplay Forensics Report</h1>\
-		     <div style='margin-left:auto;margin-right:auto;width:800px;height:200px;border:2px solid #000;'><h3>\
-		     Filename: {}<br>Case: {}<br>Timestamp: {}<br>Examiner: {}<br>Before Analysis:<br>MD5: {}<br>SHA256: {}<br>After Analysis:<br>MD5: {}<br>SHA256: {}</h3></div>".format(foldername,case,timestamp,examiner,md5,sha256,check_md5,check_sha256))
+	f.write("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width'><title>Apple Carplay</title>\
+			<link href='style.css' rel='stylesheet' type='text/css' /></head><body><h1>Apple Carplay Forensics Report</h1>\
+			<div style='margin-left:auto;margin-right:auto;width:800px;height:200px;border:2px solid #000;'><h3>\
+			Filename: {}<br>Case: {}<br>Timestamp: {}<br>Examiner: {}<br>Before Analysis:<br>MD5: {}<br>SHA256: {}<br>After Analysis:<br>MD5: {}<br>SHA256: {}</h3></div>".format(foldername,case,timestamp,examiner,md5,sha256,check_md5,check_sha256))
 
 def setup(file):
 	global case,examiner,md5,sha256,foldername,timestamp
 	timestamp = datetime.datetime.utcnow().strftime("%m/%d/%Y-%H:%M:%S UTC")
 	case = input("Enter case #:")
 	foldername = case+"-Apple"
-	exmainer = input("Enter examiner name:")
+	examiner = input("Enter examiner name:")
 	md5 = hashlib.md5(open(file,'rb').read()).hexdigest()
 	print("MD5:",md5)
 	sha256 = hashlib.sha256(open(file,'rb').read()).hexdigest()
@@ -293,14 +382,14 @@ def apple(file,user_list=None):
 		extract(file,user_list)
 		cmds = voice_commands()
 		pair = carplay_pairings()
+		app = settings()
+		cont = contacts()
 		call = call_history()
-		sms = sms_history()	
+		sms = sms_history()
 		hash_check(file)
-		report(cmds,pair,call,sms)
+		report(cmds,pair,app,cont,call,sms)
 	else:
 		setup(file)
 		extract(file,user_list)
 		hash_check(file)
 		report2()
-
-
